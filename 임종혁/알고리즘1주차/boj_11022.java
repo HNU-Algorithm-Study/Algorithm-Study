101cc3c9a9ccd0b95795430b18f5ae9b3ad0209b
limjongheok <jjongbbang2@naver.com>
2023-10-14 21:47:05 +0900

    boj_11022

--- 임종혁/알고리즘1주차/boj_11022.java
+++ 임종혁/알고리즘1주차/boj_11022.java
@@ -1,4 +1,26 @@
 package 알고리즘1주차;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class boj_11022 {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st ;
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=n; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+
+
+            System.out.println("Case #"+i+":"+" "+a+" + "+b+" = "+(a+b));
+
+        }
+
+
+    }
 }
