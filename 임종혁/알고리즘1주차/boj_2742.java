f6f44f8e46d9bf8760bca708266b1f3988cb87fa
limjongheok <jjongbbang2@naver.com>
2023-10-15 00:03:30 +0900

    boj_2742

---
+++ 임종혁/알고리즘1주차/boj_2742.java
@@ -0,0 +1,14 @@
+package 알고리즘1주차;
+
+import java.io.*;
+public class boj_2742 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i=n; i>0; i--){
+            System.out.println(i);
+        }
+
+
+    }
+}
