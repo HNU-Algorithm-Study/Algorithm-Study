274718bc206418d988c3943fc1159f657c959063
unknown <rosa0000918@gmail.com>
2023-10-14 22:36:19 +0900

    1주차

---
+++ 성효경/1주차/Boj_1157.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_1157 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int[] count = new int[26];
+
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (c<91)
+                count[c-65]++;
+            else
+                count[c-97]++;
+        }
+        int max = -1;
+        char ch = '?';
+        for (int i=0; i<26; i++) {
+            if (max < count[i]) {
+                max = count[i];
+                ch = (char)(i+65);
+            }
+            else if (max == count[i]) {
+                ch = '?';
+            }
+        }
+        System.out.println(ch);
+    }
+}
