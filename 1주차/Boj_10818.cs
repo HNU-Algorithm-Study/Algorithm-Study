8858b7d5f1176709b8aa17bf559ac5e9adfaf478
leejihye <dlwlgp8744@naver.com>
2023-10-18 13:42:14 +0900

    1주차

---
+++ 1주차/Boj_10818.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Boj_10818(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        string[] input = Console.ReadLine().Split();
+
+        int min = int.MaxValue; // 최솟값 초기값 설정
+        int max = int.MinValue; // 최댓값 초기값 설정
+
+        for (int i = 0; i < N; i++)
+        {
+            int num = int.Parse(input[i]);
+
+            if (num < min)
+            {
+                min = num; // 현재 숫자가 최솟값보다 작으면 갱신
+            }
+
+            if (num > max)
+            {
+                max = num; // 현재 숫자가 최댓값보다 크면 갱신
+            }
+        }
+
+        Console.WriteLine($"{min} {max}");
+    }
+}
