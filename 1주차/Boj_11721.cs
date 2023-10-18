bbcf765ff2f1ceb211f44b0f2a4023b11a00fe30
leejihye <dlwlgp8744@naver.com>
2023-10-18 13:37:12 +0900

    1주차

---
+++ 1주차/Boj_11721.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Boj_11721(string[] args)
+    {
+        string word = Console.ReadLine();
+
+        int length = word.Length;
+
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < 10 && (i + j) < length; j++)
+            {
+                Console.Write(word[i + j]);
+            }
+            Console.WriteLine();
+
+            // 다음 10개 출력을 위해 i를 10 증가
+            i += 9;
+        }
+    }
+}
