a59952284db58e4fff84a88be0f7975a460ac5af
leejihye <dlwlgp8744@naver.com>
2023-10-18 13:33:55 +0900

    1주차

---
+++ 1주차/Boj_11719.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Boj_11719(string[] args)
+    {
+        string inputLine;                      //문자열 변수 선언문
+
+        while ((inputLine = Console.ReadLine()) != null)
+        {
+            Console.WriteLine(inputLine);      //입력받은 문자열 그대로 출력
+        }
+    }
+}
