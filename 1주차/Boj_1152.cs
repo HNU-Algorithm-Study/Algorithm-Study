084706070d31b556b0e558f26b91e1fef7113868
leejihye <dlwlgp8744@naver.com>
2023-10-18 14:44:32 +0900

    1주차

---
+++ 1주차/Boj_1152.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Boj_1152(string[] args)
+    {
+        string input = Console.ReadLine();
+        string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        int wordCount = words.Length;
+        Console.WriteLine(wordCount);
+    }
+}
+
