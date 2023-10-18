c6f0528d0fe144e5bef452c84615e66d720a041d
leejihye <dlwlgp8744@naver.com>
2023-10-18 13:40:21 +0900

    1주차

---
+++ 1주차/Boj_2739.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Boj_2739(string[] args)
+    {
+        // N을 입력 받음
+        int N = int.Parse(Console.ReadLine());
+
+        if (N >= 1 && N <= 9)
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                int result = N * i;
+                Console.WriteLine($"{N} * {i} = {result}");
+            }
+        }
+    }
+}
