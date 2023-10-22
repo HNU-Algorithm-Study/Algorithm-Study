287cccdfca59aa21e88f0c08c62a1b8446335bdd
unknown <rosa0000918@gmail.com>
2023-10-22 23:27:30 +0900

    2주차

---
+++ 성효경/2주차/Boj_15829.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.math.*;
+
+public class Boj_15829 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int l = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+        BigInteger n = new BigInteger("0");
+        for (int i=0; i<l; i++) {
+            n = n.add(BigInteger.valueOf(31).pow(i).multiply(BigInteger.valueOf(s.charAt(i) - 96)));
+        }
+
+        n = n.mod(BigInteger.valueOf(1234567891));
+        System.out.println(n);
+    }
+}
